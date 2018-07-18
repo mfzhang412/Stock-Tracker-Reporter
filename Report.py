13ec28e94cc75038c24c09f6c751d09adf5f5b6f
mfzhang412 <mfzhang412@users.noreply.github.com>
2018-07-18 02:13:28 -0500

    Added many methods

--- Report.py
+++ Report.py
@@ -3,12 +3,64 @@ from News import News
 from Portfolios import Portfolios
 
 class Report:
-    '''This class creates relevant company reports for stock market investment'''
+    "””This class handles the creation of a formatted report and populating it with information”””
 
-    def create_report(self):
-        '''Create a report'''
+    def gather_company_data():
+        "””Gather and analyze data relevant to an evaluation of a company’s wholistic value”””
         pass
     
-    def save_report(self):
-        '''Save the report'''
+    def gather_portfolio_data():
+        "””Gather and analyze data from a top earning stock portfolio relevant to a specific company”””
+        pass
+    
+    def gather_news_data():
+        "””Gather and analyze data from recent news relevant to a specific company”””
+        pass
+    
+    def select_template():
+        "””Select a specific format for the report”””
+        pass
+    
+    def run_ranking_algorithm():
+        "””Run a ranking algorithm for a given information’s importance relevant to other information”””
+        pass
+    
+    def populate_template():
+        "””Write data to a template and create a readable report”””
+        pass
+    
+    def log_report():
+        "””Create an automatically generated file name to store the report under”””
+        pass
+    
+    def store_report():
+        "””Save report to a designated folder”””
+        pass
+    
+    def load_report():
+        "””Get report from a designated location”””
+        pass
+    
+    def wait_for_time():
+        "””Wait until scheduled time”””
+        pass
+    
+    def issue_report():
+        "””Send report via a selected method of communication”””
+        pass
+    
+    def gather_data_chunks():
+        "””Gather relevant data to populate report”””
+        pass
+    
+    def build_report():
+        "””Build a report with relevant information in the format of a specified template”””
+        pass
+    
+    def save_report():
+        """Log and save report to a designated folder"""
+        pass
+    
+    def get():
+        """Standard object attribute get methods"""
         pass
