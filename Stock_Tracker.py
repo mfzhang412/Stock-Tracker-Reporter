94051a179b5cf1270032db8baeb0713d59c39b9e
mfzhang412 <mfzhang412@users.noreply.github.com>
2018-07-18 00:03:28 -0500

    Created workflow overview

--- Stock_Tracker.py
+++ Stock_Tracker.py
@@ -3,6 +3,17 @@ from Report import Report
 
 class Stock_Tracker:
     '''This class tracks and sends daily reports on specific stocks using web scraping'''
+    def __init__(self):
+        '''Initialize a Stock_Tracker object'''
+        pass
+
+    def create_report(self):
+        '''Create a formatted report and save it'''
+        pass
+
+    def send_report(self):
+        '''Send a saved report at a designated time'''
+        pass
     
     def grab_stocks(self):
         '''Grab stocks from the "Stock_Watchlist.txt" watchlist document'''
@@ -20,7 +31,9 @@ class Stock_Tracker:
 
 def main():
     '''Main function'''
-    pass
+    tracker = Stock_Tracker()
+    tracker.create_report()
+    tracker.send_report()
 
 if __name__ == "__main__":
     main()
