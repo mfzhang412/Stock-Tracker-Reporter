4d28d5d556b86f06924a6269a00fcbc13dc8a739
mfzhang412 <mfzhang412@users.noreply.github.com>
2018-07-19 02:16:57 -0500

    Added functions

--- News.py
+++ News.py
@@ -1,8 +1,13 @@
 from Data_Analysis_Interface import Data_Analysis_Interface as DAI
 
 class News(DAI):
-    """This class handles data acquisition, data analysis, and data storage related to recent news"""
-
+    """This class handles data acquisition, data analysis, and data storage related to a recent news article"""
+    rel_data = None
+    
+    def __init__(self,data_list):
+        """Initialize News object"""
+        this.rel_dat = data_list
+    
     def scrape_web(self):
         """Access websites from save file and scrape them for relevant data"""
         pass
@@ -19,6 +24,10 @@ class News(DAI):
         """Store new data with the old data"""
         pass
 
+    def set(self):
+        """Set functions: information needed to perform tasks related to scraping, storing, and anlyzing data"""
+        pass
+
     def get(self):
-        """Get functions: ways to get the new and old data (as strings/well formatted chunks) so one can populate report with information by calling get functions"""
+        """Get functions: ways to get the new and old data from save files (as strings/well formatted chunks) so one can populate report with information by calling get functions"""
         pass
