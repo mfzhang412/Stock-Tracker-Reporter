8bdd97227fb37680d15c621cf811b3046a79ca9d
mfzhang412 <mfzhang412@users.noreply.github.com>
2018-07-18 23:14:57 -0500

    Wrote functions and flow

--- Stock_Tracker.py
+++ Stock_Tracker.py
@@ -3,31 +3,69 @@ from Report import Report
 
 class Stock_Tracker:
     """This class automates tracking specific stocks and sending daily reports"""
+    ### Instance variables
+    # stock_list = []
 
-    def __init__(self):
+    def __init__(self,file_loc='default'):
         """Initialize a Stock_Tracker object"""
-        pass
+        # gets the stocks currently in the file
+        this.stock_list = this.grab_stocks(file_loc)
 
-    def grab_stocks(self):
+    def grab_stocks(self,file_name='default'):
         """Return stocks recorded in text file document"""
-        pass
+        # open watchlist text document in read mode
+        file = open(file_name,'r')
+        # read and store contents into a list
+        stocks = []
+        if file.mode == 'r':
+            lines = file.readlines()
+            for i in range(len(lines)):
+                temp = lines[i].split('\n')
+                if ('\n' in temp): del temp[temp.index('\n')]
+                stocks.append(temp)
+        else:
+            print("File not found")
+        # close watchlist text document
+        file.close()
+        # return list
+        return stocks
 
-    def create_report(self):
+    def create_report(self,template='default',save_loc='default'):
         """Create a formatted report with relevant information and save it"""
-        pass
+        # create Report object with relevant company, portfolio, and news data
+        report = Report()
+        #####report.gather_data_chunks()    should be part of Report __init__
+        # populate report in the format of a chosen template
+        report.build_report(template)
+        # save report in specified location
+        save_log = report.save_report(save_loc)
+        # return the log of the saved report
+        return save_log
 
-    def send_report(self):
+    def send_report(self,report_loc,time='default',comm_option='default'):
         """Load and send a saved report at a scheduled time"""
-        pass
+        # load report from location
+        report = load_report(report_loc)
+        # wait until scheduled time
+        report.wait_function(time)
+        # send report through the option chosen
+        send_log = report.issue_report(comm_option)
+        # return the log of the sent report
+        return send_log
+
 
 
 
 def main():
     """Main function"""
-    tracker = Stock_Tracker()
-    tracker.grab_stocks()
-    tracker.create_report()
-    tracker.send_report()
+    # create Stock_Tracker object with a list containing companies and their stock symbols
+    tracker = Stock_Tracker("<watchlist file path>")
+    # create a report formatted based on a chosen template
+    create_log = tracker.create_report("<template choice>","<save location>")
+    # send a report at a specified time with a chosen communication method
+    send_log = tracker.send_report("<report to be sent loation>","<scheduled time>","<communication method>")
+    if send_log != "": print("Report succesfully sent")
+    else: print("Report unsuccessfully sent")
 
 if __name__ == "__main__":
     main()
